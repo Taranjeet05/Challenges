8263f4eca9b48b823e77c341fbc519f4309622dd
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-01 12:00:11 +0200

    Done

---
+++ javascript/Tip-Calculator/js/index.js
@@ -0,0 +1,20 @@
+// Initialize a assign values to 2 variables:
+//1. A variable which will hold the value of the meal cost.
+//2. A variable which will hold the value of the tip percentage.
+
+const mealCost = 10; // 10 euros
+const tipPercentage = 2 // 5 percent tip
+
+
+// calculate tip amount
+
+let tipAmount = mealCost * tipPercentage;
+console.log(tipAmount);
+
+// calculate totalcost
+
+let totalCost = mealCost + tipAmount;
+console.log(totalCost);
+
+console.log (`Total cost os your bill is ${totalCost} euros`);
+
