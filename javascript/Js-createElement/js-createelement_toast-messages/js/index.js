3b8fc520da9700db702a0477ecb38319c4cb2a14
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-08 12:24:12 +0200

    Done .

---
+++ javascript/Js-createElement/js-createelement_toast-messages/js/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+const toastContainer = document.querySelector('[data-js="toast-container"]');
+const addButton = document.querySelector('[data-js="add-button"]');
+const clearButton = document.querySelector('[data-js="clear-button"]');
+
+addButton.addEventListener("click", () => {
+  const newToast = document.createElement('li');
+  newToast.classList.add('toast-container__message');
+
+  newToast.textContent = "I'm a new toast message.";
+
+  toastContainer.appendChild(newToast);
+});
+
+clearButton.addEventListener("click", () => {
+  
+  toastContainer.innerHTML = '';
+});
