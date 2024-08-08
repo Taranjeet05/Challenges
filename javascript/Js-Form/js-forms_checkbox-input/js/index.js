c03f5840426c38b4434d05003fdb96edd0f31279
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-08 15:36:44 +0200

    done .

---
+++ javascript/Js-Form/js-forms_checkbox-input/js/index.js
@@ -0,0 +1,37 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const tosError = document.querySelector('[data-js="tos-error"]');
+const tosCheckbox = document.querySelector('[data-js="tos"]');
+
+function hideTosError() {
+  tosError.setAttribute("hidden", "");
+}
+
+function showTosError() {
+  tosError.removeAttribute("hidden");
+}
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!tosCheckbox.checked) {
+    showTosError(); 
+    return; 
+  }
+
+
+  alert("Form submitted");
+
+
+  form.reset();
+  form.firstName.focus();
+});
