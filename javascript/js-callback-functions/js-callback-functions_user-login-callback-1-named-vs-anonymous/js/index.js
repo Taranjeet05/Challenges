6575964676c3d39ee7c26e94161ec2bb0c3054a9
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-13 10:22:38 +0200

    done .

---
+++ javascript/js-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+
+
+handleUserLogin(function() {
+  console.log('User logged in successfully!');
+});
