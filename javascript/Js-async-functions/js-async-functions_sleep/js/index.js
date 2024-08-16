67f80d9f60d9ddbda996438b734ff846c8714adc
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-16 11:36:28 +0200

    Solved the Sleep challenge

---
+++ javascript/Js-async-functions/js-async-functions_sleep/js/index.js
@@ -0,0 +1,47 @@
+const icon = document.querySelector('[data-js="icon"]');
+const button3 = document.querySelector('[data-js="sleep-3-button"]');
+const button5 = document.querySelector('[data-js="sleep-5-button"]');
+const button10 = document.querySelector('[data-js="sleep-10-button"]');
+const body = document.body;
+
+// --v-- code here --v--
+
+// Button 1 (3 seconds) 
+button3.addEventListener("click", async () => {
+  icon.textContent = "💤";
+  body.classList.add("dark");   
+  await sleep(3);               
+  icon.textContent = "⏰";
+  body.classList.remove("dark"); 
+});
+
+// Button 2 (5 seconds) 
+button5.addEventListener("click", async () => {
+  icon.textContent = "💤";
+  body.classList.add("dark");   
+  await sleep(5);               
+  icon.textContent = "⏰";
+  body.classList.remove("dark"); 
+});
+
+// Button 3 (10 seconds) 
+function handleSleepFor10Button() {
+  icon.textContent = "💤";
+  body.classList.add("dark");   
+  sleep(10).then(() => {        
+    icon.textContent = "⏰";
+    body.classList.remove("dark"); 
+  });
+}
+
+button10.addEventListener("click", handleSleepFor10Button);
+
+// --^-- code here --^--
+
+// returns a Promise that resolves after x seconds
+function sleep(seconds) {
+  const sleepPromise = new Promise((resolve) => {
+    setTimeout(() => resolve(), seconds * 1000);
+  });
+  return sleepPromise;
+}
