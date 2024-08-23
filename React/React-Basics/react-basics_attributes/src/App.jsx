89f47ef18a39615e9d45430906ef5d9c010d00ae
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-08-23 16:08:21 +0200

    Solved this challenge

---
+++ React/React-Basics/react-basics_attributes/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+export default function App() {
+  return <Article/>;
+}
+
+/*## Task
+
+Write an `Article` component and use it to replace the given `<div>`.
+
+Your component should contain the following parts:
+
+- an `<article>` as a wrapper HTML element with the class `article`
+- an `<h2>` with the class `article__title` and a text of your choice
+- a `<label>` and `<input>` tag connected with `id` and `htmlFor` (!) attributes
+- an `<a>` tag with the class `article__link` as well as text content and 
+ `href` attribute of your choice (What about a Wikipedia article?)
+ */
+
+ function Article () {
+  return (
+   <article class ="article">
+    <h2 class ="article__title">Hello Everyone to learn React </h2>
+    <label htmlFor="react"><input type="text" id ="input--tag" /></label>
+    <a href="https://github.com/" class = "article__link"></a>
+   </article>
+  );
+ }
