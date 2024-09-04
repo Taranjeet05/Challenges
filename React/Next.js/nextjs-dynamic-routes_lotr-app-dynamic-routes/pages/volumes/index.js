8f441f39264ffb9f874bf495df99dc4e1c8bfdd4
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-04 16:59:41 +0200

    done

---
+++ React/Next.js/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { introduction, volumes } from '../../lib/data';
+
+export default function Volumes() {
+  const router = useRouter();
+
+  function handleRandomVolume() {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <ul>
+        {volumes.map(volume => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>
+              {volume.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleRandomVolume}>
+        Random Volume
+      </button>
+    </>
+  );
+}
+
