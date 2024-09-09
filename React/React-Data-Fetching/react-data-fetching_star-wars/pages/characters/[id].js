b506daae4c7ba5c038d9fb42ca5b7cfd99c7390a
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-09 18:04:33 +0200

    Fetch Star Wars character with SWR, added dynamic route support.

---
+++ React/React-Data-Fetching/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error } = useSWR(
+    id ? `https://swapi.py4e.com/api/people/${id}` : null,
+    fetcher
+  );
+
+  if (error) return <div>Error loading character.</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
