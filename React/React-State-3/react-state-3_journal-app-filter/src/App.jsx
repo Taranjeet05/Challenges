d9c54174f1d78be26a8e930c397603a05601b448
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-28 20:00:35 +0200

    Add filter functionality for journal entries and favorite toggling

--- React/React-State-3/react-state-3_journal-app-filter/src/App.jsx
+++ React/React-State-3/react-state-3_journal-app-filter/src/App.jsx
@@ -11,38 +11,38 @@ const initialEntries = [
     id: 1000,
     date: "Feb 5, 2025",
     motto: "We are in a state of chaos",
-    notes:
-      "Today I learned about React State. It was fun! I can't wait to learn more.",
+    notes: "Today I learned about React State. It was fun! I can't wait to learn more.",
+    isFavorite: true,
   },
   {
     id: 999,
     date: "Feb 4, 2025",
     motto: "Props, Props, Props",
-    notes:
-      "Today I learned about React Props. Mad props to everyone who understands this!",
+    notes: "Today I learned about React Props. Mad props to everyone who understands this!",
+    isFavorite: false,
   },
   {
     id: 998,
     date: "Feb 3, 2025",
     motto: "How to nest components online fast",
-    notes:
-      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    notes: "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    isFavorite: true,
   },
   {
     id: 997,
     date: "Feb 2, 2025",
     motto: "I'm a React Developer",
     notes: "My React-ion when I learned about React: Yay!",
+    isFavorite: false,
   },
 ];
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
+  const [filter, setFilter] = useState("all"); 
 
   function handleAddEntry(newEntry) {
-    const date = new Date().toLocaleDateString("en-us", {
-      dateStyle: "medium",
-    });
+    const date = new Date().toLocaleDateString("en-us", { dateStyle: "medium" });
     setEntries([{ id: uid(), date, ...newEntry }, ...entries]);
   }
 
@@ -54,14 +54,29 @@ function App() {
     );
   }
 
+  function handleShowAllEntries() {
+    setFilter("all");
+  }
+
+  function handleShowFavoriteEntries() {
+    setFilter("favorites");
+  }
+
+  const favoriteEntries = entries.filter((entry) => entry.isFavorite); 
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
         <EntryForm onAddEntry={handleAddEntry} />
         <EntriesSection
-          entries={entries}
+          entries={filter === "all" ? entries : favoriteEntries} 
           onToggleFavorite={handleToggleFavorite}
+          onShowAllEntries={handleShowAllEntries} 
+          onShowFavoriteEntries={handleShowFavoriteEntries} 
+          filter={filter} 
+          allEntriesCount={entries.length} 
+          favoriteEntriesCount={favoriteEntries.length} 
         />
       </main>
       <Footer />
