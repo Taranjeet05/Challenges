d5112b8fe792d650a228fe7cb52c56f45459c3ca
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-28 20:18:12 +0200

    Lift state, implement entry addition

--- React/React-State-3/react-state-3_journal-app-form/src/App.jsx
+++ React/React-State-3/react-state-3_journal-app-form/src/App.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import "./App.css";
+import { uid } from "uid"; 
+
+
+const initialEntries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes: "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+];
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  function handleAddEntry(newEntry) {
+    const date = new Date().toLocaleDateString("en-us", { dateStyle: "medium" });
+    const entryWithId = { id: uid(), date, ...newEntry };
+    setEntries([entryWithId, ...entries]); 
+  }
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
+        <EntryForm onAddEntry={handleAddEntry} /> 
+        <EntriesSection entries={entries} /> 
       </main>
       <Footer />
     </div>
--- React/React-State-3/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
+++ React/React-State-3/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  const [motto, setMotto] = useState("");
+  const [notes, setNotes] = useState("");
+
+  
+  function handleSubmit(event) {
+    event.preventDefault(); 
+    const newEntry = { motto, notes }; 
+    onAddEntry(newEntry); 
+    setMotto(""); 
+    setNotes("");
+  }
+
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
           <label htmlFor="motto">Motto</label>
-          <input type="text" name="motto" id="motto" />
+          <input
+            type="text"
+            name="motto"
+            id="motto"
+            value={motto}
+            onChange={(event) => setMotto(event.target.value)}
+          />
         </div>
         <div className="entry-form__field">
           <label htmlFor="notes">Notes</label>
-          <textarea name="notes" id="notes" rows="4" />
+          <textarea
+            name="notes"
+            id="notes"
+            rows="4"
+            value={notes}
+            onChange={(event) => setNotes(event.target.value)}
+          />
         </div>
         <div className="entry-form__button-wrapper">
           <Button type="submit">Create</Button>
